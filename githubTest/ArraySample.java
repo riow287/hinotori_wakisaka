f18646efb30b6cdd2a0fd67f29407e137cf86795
riow287 <141637793+riow287@users.noreply.github.com>
2024-06-28 16:54:54 +0900

    Add files via upload

---
+++ githubTest/ArraySample.java
@@ -0,0 +1,32 @@
+ // test_jyukoPC_01
+
+ // test_lenovoPC_01
+
+ // test_lenovoPC_02
+
+ // test_01
+ 
+
+/**
+ * ArraySample
+ * @author emBex Education
+ */
+class ArraySample {
+	/**
+	 * main method
+	 * @param args : args data
+	 */	
+	public static void main(String[] args) {
+
+        int[] array = new int[5];
+
+		array[0] = 10;
+		array[1] = 4;
+		array[2] = 1;
+		array[3] = 5;
+		array[4] = 7;
+
+		System.out.println("first element:" + array[0]);
+		System.out.println("num of element:" + array.length);   
+    }
+}
